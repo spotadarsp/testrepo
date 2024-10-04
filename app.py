26794a39b2b816d3370fed5f222dd454f9dfa27a
shubham <spotadar.sp@gmail.com>
2024-10-04 16:38:33 +0530

    chnaged hoest name

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pymysql
 app = Flask(__name__)
 
 def get_db_connection():
-    connection = pymysql.connect(host='mydb.cylck8yh5jkc.eu-central-1.rds.amazonaws.com',  # Replace with your RDS endpoint
+    connection = pymysql.connect(host='mydb.cr62aicms8kq.ap-south-1.rds.amazonaws.com',  # Replace with your RDS endpoint
                                  user='dbuser',      # Replace with your RDS username
                                  password='dbpassword',  # Replace with your RDS password
                                  db='devprojdb',   # Replace with your database name
